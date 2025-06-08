fed131a77100ea3eec1166165c6aac45ff0b33e6
achibhossengit <mail.achibhossen@gmail.com>
2025-06-08 18:06:07 +0600

    setup tailwind, daisyui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <p className='text-xl font-bold bg-green-300'>Hello jobseeker. We are here to solve your problem.</p>
+      <button className="btn btn-secondary">Click Me</button>
+    </>
+ 
+)
+}
+
+export default App
