aedcd8ab9ac869f19e44d215cb5915f126e2d6dc
achibhossengit <mail.achibhossen@gmail.com>
2026-09-13 08:00:48 +0600

    feat: load api base url from vite env

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'https://work-way.vercel.app/api/v1/'
-    // baseURL: 'http://127.0.0.1:8000/api/v1/'
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
 export default apiClient;
